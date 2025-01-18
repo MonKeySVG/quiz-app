738d890d392b1b3bd6536932f43621638f3db7f1
MonKeySVG <play95100@gmail.com>
2025-01-18 11:12:47 +0100

    Début des modifications de style

--- quiz-ui/src/App.vue
+++ quiz-ui/src/App.vue
@@ -8,8 +8,8 @@ import { RouterLink, RouterView } from 'vue-router'
     <div class="wrapper">
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/" class="navlink shadow">Home</RouterLink>
+        <RouterLink to="/about" class="navlink shadow">About</RouterLink>
       </nav>
     </div>
   </header>
@@ -18,65 +18,30 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+template {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  display: flex;
+  gap: 20px;
+  justify-content: center;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.navlink  {
+  text-decoration: none;
+  color: white;
+  background-color: #333;
+  font-size: 1.2rem;
+  padding: 6px 24px;
+  border-radius: 4px;
+
+  
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
 
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
